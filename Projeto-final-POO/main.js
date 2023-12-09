36408fa8ebf217fb118c40e65f038017b69b74b3
Rodr1goPA <pereira87pa@gmail.com>
2023-12-08 22:27:04 -0300

    update projeto final

---
+++ Projeto-final-POO/main.js
@@ -0,0 +1,36 @@
+import { CursoPresencial } from './CursoPresencial.js'
+import { CursoOnline } from './CursoOnline.js'
+import { Trilha } from './Trilha.js'
+
+const curso1 = new CursoOnline(
+  'javascript',
+  '300hs',
+  1000.0,
+  'http://teste.com.br'
+)
+
+const curso2 = new CursoPresencial(
+  'HTML/CSS',
+  '200hs',
+  499,
+  'http://teste.com.br'
+)
+
+
+const cursoEdit = new CursoOnline(
+  'javascript',
+  '300hs',
+  399,
+  'http://teste.com.br'
+)
+cursoEdit.id = 1
+
+Trilha.adicionarCurso(curso1)
+Trilha.adicionarCurso(curso2)
+console.log(Trilha.cursos)
+
+Trilha.deletarCurso(1)
+console.log(Trilha.cursos)
+
+Trilha.atualizarProduto(cursoEdit)
+console.log(Trilha.cursos)
